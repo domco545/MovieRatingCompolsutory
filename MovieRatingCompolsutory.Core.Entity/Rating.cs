c3128596ab99a4f8eab219a5495a5c4559a36942
Dominik Rothbauer <dominikrothbauer@gmail.com>
2020-09-25 00:25:05 +0200

    added service interface and service calss and implemented rating class

--- MovieRatingCompolsutory.Core.Entity/Rating.cs
+++ MovieRatingCompolsutory.Core.Entity/Rating.cs
@@ -6,6 +6,9 @@ namespace MovieRatingCompolsutory.Core.Entity
 {
     public class Rating
     {
-
+        public int Reviewer { get; set; }
+        public int Movie { get; set; }
+        public int Grade { get; set; }
+        public DateTime Date { get; set; }
     }
 }
