cf8ac8687db68fbc3ee6f18d0a9299eb6f951fa6
Dominik Rothbauer <dominikrothbauer@gmail.com>
2020-10-05 11:42:47 +0200

    added tests

--- MovieRatingPerformanceTest/UnitTest1.cs
+++ MovieRatingPerformanceTest/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace MovieRatingPerformanceTest
     public class UnitTest1
     {
         static IRatingService service;
-        /*
+        
         [ClassInitialize]
         public static void Initialize(TestContext context)
         {
@@ -22,21 +22,36 @@ namespace MovieRatingPerformanceTest
             service = new RatingServiceFile(repo.Object);
             repo.Verify(m => m.GetAll(), Times.Once);
         }
-        */
+
+        /*
         [TestMethod]
         [Timeout(40000)]
         public void LoadJson() 
         {
            var JsonList = new RatingsRepository().GetAll();
         }
+        */
 
-        /*
+        
         [TestMethod]
         [Timeout(4000)]
         public void GetNumberOfReviewsFromReviewerPerformance()
         {
             service.GetNumberOfReviewsFromReviewer(25);
         }
-        */
+
+        [TestMethod]
+        [Timeout(4000)]
+        public void GetAverageRateFromReviewerPerformance()
+        {
+            service.GetAverageRateFromReviewer(25);
+        }
+
+        [TestMethod]
+        [Timeout(4000)]
+        public void GetNumberOfRatesByReviewerPerformance()
+        {
+            service.GetNumberOfRatesByReviewer(25, 5);
+        }
     }
 }
